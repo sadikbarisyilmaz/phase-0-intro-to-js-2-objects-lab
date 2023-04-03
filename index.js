e229d22a23d77d1d56384ed63977b326d79194b2
Sadık Barış Yılmaz <sadikbarisyilmaz@gmail.com>
2023-04-03 19:39:39 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,26 @@
 // Write your solution in this file!
+const employee = {
+    name: "baris" ,
+    street: "108" ,
+}
+
+const updateEmployeeWithKeyAndValue = (employee, key, value) => {
+    const newEmployee = {...employee, [key]: value}
+    return newEmployee
+}
+
+const destructivelyUpdateEmployeeWithKeyAndValue = (employee, key, value) => {
+    employee[key] = value
+    return employee
+}
+
+const deleteFromEmployeeByKey = (employee, key) => {
+    const newEmployee = {...employee}
+    delete newEmployee[key]
+    return newEmployee
+}
+
+const destructivelyDeleteFromEmployeeByKey = (employee, key) => {
+    delete employee[key]
+    return employee
+}
